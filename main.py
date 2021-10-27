037590fbfe91c8b471a2b24b8de9429fec01d4e3
Jiaxin-yyjx <80301995+Jiaxin-yyjx@users.noreply.github.com>
2021-10-27 19:47:38 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -21,44 +21,51 @@ term_l = {}
 # get the stop words list
 sw_l = stopwords('en')
 
-txt = open("patent_40.txt", "r")
-txt = str(txt.read())
 
+# read filename file
+with open('filepath','r') as f:
+    txtName = f.readline()
+    while txtName:
+        txt = open(txtName + ".txt", "r")
+        txt = str(txt.read())
 
-# get the phrases (without removing stop words from original document) and their frequency count
-nlp = stanza.Pipeline(lang='en', processors='tokenize,mwt,pos,lemma,depparse')
-doc = nlp(txt)
-for sent in doc.sentences:
-    for word in sent.words:
-        if word.deprel == "amod":
-            phrase = word.text.lower() + "_" + sent.words[word.head-1].text.lower()
-            if phrase in phrase_l.keys():
-                phrase_l[phrase] = phrase_l[phrase] + 1
-            else:
-                phrase_l[phrase] = 1
-        elif word.deprel == "compound" or word.deprel =="flat" or word.deprel=="xcomp":
-            phrase = sent.words[word.head-1].text.lower() + "_" + word.text.lower()
-            if phrase in phrase_l.keys():
-                phrase_l[phrase] = phrase_l[phrase] + 1
-            else:
-                phrase_l[phrase] = 1
+        # get the phrases (without removing stop words from original document) and their frequency count
+        nlp = stanza.Pipeline(lang='en', processors='tokenize,mwt,pos,lemma,depparse')
+        doc = nlp(txt)
+        for sent in doc.sentences:
+            for word in sent.words:
+                if word.deprel == "amod":
+                    phrase = word.text.lower() + "_" + sent.words[word.head - 1].text.lower()
+                    if phrase in phrase_l.keys():
+                        phrase_l[phrase] = phrase_l[phrase] + 1
+                    else:
+                        phrase_l[phrase] = 1
+                elif word.deprel == "compound" or word.deprel == "flat" or word.deprel == "xcomp":
+                    phrase = sent.words[word.head - 1].text.lower() + "_" + word.text.lower()
+                    if phrase in phrase_l.keys():
+                        phrase_l[phrase] = phrase_l[phrase] + 1
+                    else:
+                        phrase_l[phrase] = 1
 
-# get the words (that is not a stop word) and their frequency count
-txt = txt.translate(str.maketrans('', '', string.punctuation))
-txt = txt.split(" ")
-num_l = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-for word in txt:
-    word = word.lower()
-    if word not in sw_l and word not in num_l:
-        if word in term_l.keys():
-            term_l[word] = term_l[word] + 1
-        else:
-            term_l[word] = 1
+        # get the words (that is not a stop word) and their frequency count
+        txt = txt.translate(str.maketrans('', '', string.punctuation))
+        txt = txt.split(" ")
+        num_l = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        for word in txt:
+            word = word.lower()
+            if word not in sw_l and word not in num_l:
+                if word in term_l.keys():
+                    term_l[word] = term_l[word] + 1
+                else:
+                    term_l[word] = 1
 
-# print result
-print(dict(sorted(phrase_l.items(), key=operator.itemgetter(1), reverse=True)))
-print(dict(sorted(term_l.items(), key=operator.itemgetter(1), reverse=True)))
-term_l_cp = dict(sorted(term_l.items(), key=operator.itemgetter(1), reverse=True))
+        # print result
+        print(dict(sorted(phrase_l.items(), key=operator.itemgetter(1), reverse=True)))
+        print(dict(sorted(term_l.items(), key=operator.itemgetter(1), reverse=True)))
+        term_l_cp = dict(sorted(term_l.items(), key=operator.itemgetter(1), reverse=True))
+
+        txtName = f.readline()
+
 
 # remove term whose frequency is in the top 5% among all
 # num_remove = len(term_l)*0.05
