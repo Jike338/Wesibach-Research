98aac55a4ea234827e21db8e26f039828da0c567
Jike Zhong <jike_338@Jikes-MBP.attlocal.net>
2021-11-07 17:00:04 -0500

    adding result

--- lemmatization.py
+++ lemmatization.py
@@ -16,12 +16,13 @@ def listToString(s):
     return (str1.join(s))
 
 # to read data in a single excel file
-csv = pd.read_csv("31.csv").dropna()
+csv = pd.read_csv("31.csv")
 csv['all'] = csv[csv.columns[1:]].apply(
     lambda x: ' '.join(x.dropna().astype(str)),
     axis=1
 )
 new_csv = csv['wku']+csv['all']
+new_csv = new_csv.dropna()
 last_doc = ""
 tokenization_l = []
 for num, txt in enumerate(new_csv):
@@ -32,7 +33,7 @@ for num, txt in enumerate(new_csv):
     else:
         curr_doc = last_doc + " " + curr_doc
     tokenization = nltk.word_tokenize(curr_doc) 
-    tokenization_l.append(listToString(tokenization)) 
+    tokenization_l.append(listToString(tokenization))
 
 df = pd.DataFrame(tokenization_l, columns=['value'])
 df.to_csv('res2.csv', index=False)
