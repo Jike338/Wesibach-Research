6ec38f538a55223eae6bd0ecfea4acaf9042d306
Jike Zhong <jike_338@Jikes-MBP.attlocal.net>
2021-11-07 16:28:59 -0500

    adding test file

--- lemmatization.py
+++ lemmatization.py
@@ -1,6 +1,7 @@
 import nltk
 from nltk.stem import 	WordNetLemmatizer
 import pandas as pd
+from csv import DictReader
 
 nltk.download('punkt')
 nltk.download('wordnet')
@@ -12,16 +13,20 @@ for w in tokenization:
 
 
 # to read data in a single excel file
-csv = pd.read_csv("test_2.csv")
+csv = pd.read_csv("31.csv")
 csv['all'] = csv[csv.columns[1:]].apply(
     lambda x: ' '.join(x.dropna().astype(str)),
     axis=1
 )
-
-new_csv = csv['all']
-
-counter =1
-for txt in new_csv:
-    print("Processing document #: " + str(counter))
-    tokenization = nltk.word_tokenize(txt)
-    print(tokenization)
+new_csv = csv['wku']+csv['all']
+last_doc = ""
+for num, txt in enumerate(new_csv):
+    print(len(txt))
+    
+    if str(txt)[0].isnumeric():
+        last_doc = txt
+    else:
+        last_doc = last_doc + " " + txt
+    print(len(last_doc))
+    print(last_doc[0])
+    tokenization = nltk.word_tokenize(last_doc)    
