d903a344c18a48038190938721b092319c027325
Jike Zhong <jike_338@Jikes-MBP.attlocal.net>
2021-11-07 16:55:12 -0500

    adding to csv files

--- lemmatization.py
+++ lemmatization.py
@@ -11,16 +11,19 @@ tokenization = nltk.word_tokenize(text)
 for w in tokenization:
     print("Lemma for {} is {}".format(w, wordnet_lemmatizer.lemmatize(w))) 
 
+def listToString(s):
+    str1 = " "
+    return (str1.join(s))
 
 # to read data in a single excel file
-csv = pd.read_csv("31.csv")
+csv = pd.read_csv("31.csv").dropna()
 csv['all'] = csv[csv.columns[1:]].apply(
     lambda x: ' '.join(x.dropna().astype(str)),
     axis=1
 )
 new_csv = csv['wku']+csv['all']
-new_csv = new_csv.dropna()
 last_doc = ""
+tokenization_l = []
 for num, txt in enumerate(new_csv):
     curr_doc = ""
     last_doc = txt
@@ -28,5 +31,8 @@ for num, txt in enumerate(new_csv):
         curr_doc = txt
     else:
         curr_doc = last_doc + " " + curr_doc
-    tokenization = nltk.word_tokenize(curr_doc)    
-    print(tokenization)
+    tokenization = nltk.word_tokenize(curr_doc) 
+    tokenization_l.append(listToString(tokenization)) 
+
+df = pd.DataFrame(tokenization_l, columns=['value'])
+df.to_csv('res2.csv', index=False)
